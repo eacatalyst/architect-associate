b3784a6e4434a176fd8865a1a6b96d34ee68f6ee
Clayson Willis <40024980+eacatalyst@users.noreply.github.com>
2023-06-12 15:57:25 -0400

    Update 1-user-data.sh

--- week2/1-user-data.sh
+++ week2/1-user-data.sh
@@ -1,1 +1,6 @@
-
+#!/bin/bash
+yum update -y
+yum install -y httpd.x86_64
+systemctl start httpd.service
+systemctl enable httpd.service
+echo “Hello World from loadbalancing $(hostname -f)” > /var/www/html/index.html
