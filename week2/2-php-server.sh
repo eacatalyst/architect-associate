cb37fbfde8afa666ab44252d71ef4be78a2abf79
Clayson Willis <40024980+eacatalyst@users.noreply.github.com>
2023-06-12 15:39:31 -0400

    Update 2-php-server.sh

--- week2/2-php-server.sh
+++ week2/2-php-server.sh
@@ -1,1 +1,12 @@
-
+#!/bin/bash
+echo "updating system">>/tmp/user-data-run.log
+apt update -y
+echo "Installing package">>/tmp/user-data-run.log
+apt install -y apache2 php
+echo "Downloading code">>/tmp/user-data-run.log
+cd/tmp
+git clone https//github.com/nindate/web-pages.git
+echo "Deploying code">>/tmp/user-data-run.log
+mv web-pages/simple-site/* /var/www/html/
+rm/var/www/html/index.html
+echo "system ready to use">>/tmp/user-data-run.log
