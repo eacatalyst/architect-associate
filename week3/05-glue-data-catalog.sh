98fa1757e8599cf684df675fb2c5330004df67d6
Clayson Willis <40024980+eacatalyst@users.noreply.github.com>
2023-06-15 12:26:25 -0400

    Create 05-glue-data-catalog.sh

---
+++ week3/05-glue-data-catalog.sh
@@ -0,0 +1,13 @@
+# Create a glue crawler
+
+1 - Create a VPC
+2 - Create VPC Endpoint - s3 global services
+3 - Go to S3 portal create bucket with two folders
+	- input
+	- output
+3 - Go Glue Portal
+4 - Select Crawler
+5 - Create Crawler - put s3 bucket as source
+6 - Create a new IAM role
+7 - Run Crawler
+8 - Review execution and database (go to logs)
